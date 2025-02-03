9b555b81cd6e4174750316c37f358445569a2d25
Khushal <khushal.innovatemr@gmail.com>
2025-02-03 15:42:38 +0530

    output and Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from '../models/user';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UserProfileComponent],
+  imports: [RouterOutlet,UserProfileComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  users = [
+    {name:"Rohan",isSingle:true,salary:10000},
+    {name:"Khushal",isSingle:false,salary:20000},
+    {name:"Ronak",isSingle:true,salary:40000},
+  ]
   title = 'one';
+  recievedData(e:User){
+    console.log(e);
+    
+    const index = this.users.findIndex(user =>user.name == e.name)
+    this.users[index].salary = e.newSalary
+  }
 }
--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input, numberAttribute, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { User } from '../../../models/user';
+
+function formatName(value: string) {
+  return "" + value;
+}
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
@@ -15,30 +20,38 @@ export class UserProfileComponent {
   city = "Surat"
   company = "Software Developer"
   age = "23"
+  fees = "2700"
   isBtnDisabled = true
   inputVal = "Intern"
 
   users = [
-    {name:"Rohan",isSingle:true,salary:10000},
-    {name:"Khushal",isSingle:false,salary:20000},
-    {name:"Ronak",isSingle:true,salary:40000},
-    
+    { name: "Rohan", isSingle: true, salary: 10000 },
+    { name: "Khushal", isSingle: false, salary: 20000 },
+    { name: "Ronak", isSingle: true, salary: 40000 },
   ]
+
   interns = [
-    {name:"Dinesh",isSingle:true,salary:30000},
-    {name:"Kautilya",isSingle:false,salary:43000},
-    {name:"Yash",isSingle:false,salary:56000},
-    
+    { name: "Dinesh", isSingle: true, salary: 30000 },
+    { name: "Kautilya", isSingle: false, salary: 43000 },
+    { name: "Yash", isSingle: false, salary: 56000 },
   ]
   
-  onChange(){
+  onChange() {
     console.log("called")
   }
   
-  onChain(e:Event){
+  onChain(e: Event) {
     const value = (e.target as HTMLInputElement).value
     this.inputVal = value;
+  }
 
-  } 
+  @Input({ alias: "names", transform: formatName }) nam_e = "";
+  @Input({ transform: booleanAttribute }) isSingle!: boolean;
+  @Input({ transform: numberAttribute }) salary!: number;
+  @Input() item = 'Box';
+
+  @Output() myEvent = new EventEmitter<User>()
+  sendData() {
+    this.myEvent.emit({ name: this.nam_e, newSalary: 76884 })
+  }
 }
-

50208ce22bf14b1f8caaf87eb80c775062b03f31
Khushal <khushal.innovatemr@gmail.com>
2025-02-04 16:09:51 +0530

    Http Requests(joke)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { User } from '../models/user';
+import { JokeComponent } from './components/joke/joke.component'; 
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,UserProfileComponent,CommonModule],
+  standalone: true, 
+  imports: [RouterOutlet,UserProfileComponent,CommonModule,JokeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = [
     {name:"Rohan",isSingle:true,salary:10000},
-    // {name:"Khushal",isSingle:false,salary:20000},
-    // {name:"Ronak",isSingle:true,salary:40000},
+    {name:"Khushal",isSingle:false,salary:20000},
+    {name:"Ronak",isSingle:true,salary:40000},
   ]
   title = 'one';
   recievedData(e:User){
@@ -23,5 +24,8 @@ export class AppComponent {
     
     const index = this.users.findIndex(user =>user.name == e.name)
     this.users[index].salary = e.newSalary
+  }
+  clear(){
+    this.users = []
   }
 }
